a378619ffb77410efbc81d8f7c8fffdfb2f4eae5
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2023-10-06 00:29:46 +0300

    Update run_pentamer.sh

--- classifier/dl/run_pentamer.sh
+++ classifier/dl/run_pentamer.sh
@@ -9,7 +9,7 @@ FASTA=$1
 
 #shifter  --image=bryce911/bbtools  commonkmers.sh  -Xmx1000m -threads=1  in=$FASTA  out=stdout  k=5  display=3  count
 
-/srv/jgi-ml/classifier/dl/bbmap/commonkmers.sh  -Xmx1000m -threads=1  in=$FASTA  out=stdout  k=5  display=3  count  
+${DEEPLASMID_HOME}/bbmap/commonkmers.sh  -Xmx1000m -threads=1  in=$FASTA  out=stdout  k=5  display=3  count  
 
 #& ID=$! ; fg
 
